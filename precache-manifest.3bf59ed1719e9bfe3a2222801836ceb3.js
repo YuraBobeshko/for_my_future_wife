783306f4dd24d63cbd3e4bf2a95207d5e84e91aa
YuraBobeshko <gorobech55@gmail.com>
2019-12-02 01:21:21 +0200

    Updates

---
+++ precache-manifest.3bf59ed1719e9bfe3a2222801836ceb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3b779063474f5d4e8a4e771b8eb2c8",
+    "url": "/for_my_future_wife/index.html"
+  },
+  {
+    "revision": "fb7f885382879ee6e8ba",
+    "url": "/for_my_future_wife/static/css/main.9b7c133a.chunk.css"
+  },
+  {
+    "revision": "472b6cc1ac678ff7b28f",
+    "url": "/for_my_future_wife/static/js/2.2f4377ec.chunk.js"
+  },
+  {
+    "revision": "fb7f885382879ee6e8ba",
+    "url": "/for_my_future_wife/static/js/main.50e0a20c.chunk.js"
+  },
+  {
+    "revision": "7440a7fab133dcacfbec",
+    "url": "/for_my_future_wife/static/js/runtime-main.83455d72.js"
+  }
+]);
